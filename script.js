d24077556013ac543951a2e1bda81d013f8860b3
Viren <viren15152@gmail.com>
2023-11-22 03:46:53 +0000

    Updated script.js with more comments

--- script.js
+++ script.js
@@ -6,17 +6,20 @@ var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var special = ['!', '@', '£', '$', '%', '^', '&', '*', '#', '(', ')', '?', '<', '>', '=', '+', '_', '-', '.', '/', '`', '~', '|', ':', ';', '{', '}', ']', '['];
 
-//I've created a function that will ensure the user is requested to confirm which options they would like. I've also included conditional statements to make certain the minimum option requirements were made.
+//This function is to prompt the user for password criteria 
 
 
 function questions() {
   var isValid = false;
   do {
+
+    //This will the prompt the user for password length and character types
     var length = prompt("Select password length between 8 and 128 characters");
     var askNumbers = confirm("Would you like your password to contain numbers?");
     var askLowerCase = confirm("Would you like your password to contain lower case letters?");
     var askUpperCase = confirm("Would you like your password to contain upper case letters?");
     var askSpecial = confirm("Would you like your password to include special characters?");
+    // This section will allow me to store user responses 
     var responses = {
       length: length,
       askNumbers: askNumbers,
@@ -24,6 +27,8 @@ function questions() {
       askUpperCase: askUpperCase,
       askSpecial: askSpecial,
     };
+
+    //This section will validate user reponses 
     if (length < 8 || length > 128) {
       alert("Choose a number between 8 and 128");
     } else if (!askNumbers && !askLowerCase && !askUpperCase && !askSpecial) {
@@ -32,10 +37,11 @@ function questions() {
       isValid = true;
     }
   } while (!isValid);
+  //This will allow me to return my users responses 
   return responses;
 }
 
-// I have created this function to join all of the users responses to then create a result of a strong password as per the user story. 
+// I have created this function to join all of the users responses to then create a result of a strong password based on the users choices
 
 function generatePassword() {
   var passwordOptions = questions();
@@ -54,18 +60,18 @@ function generatePassword() {
   if (passwordOptions.askSpecial) {
     for (var i of special) possibleCombo.push(i);
   }
-
+//Log possible characters to the console 
   console.log(possibleCombo);
-//for loop (loops through a block of code a number of times.)
+//for loop (loops through a block of code a number of times.) This will generate a password randomly by selecting characters from possibleCombo
   for (var i = 0; i < passwordOptions.length; i++) {
     finalPassword += possibleCombo[Math.floor(Math.random() * possibleCombo.length)];
   }
-
+// This will log the final password to the console
   console.log(finalPassword);
 
   return finalPassword;
 }
-
+//This function will write the generated password to the page
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -73,5 +79,5 @@ function writePassword() {
   passwordText.value = password;
 }
 
-//Event listener to generate button
+//Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
